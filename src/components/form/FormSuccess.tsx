8d1f6495c39d59ec6fcc3407ba3e1298e85b50eb
keigo9 <keigo111kokoa@gmail.com>
2025-05-27 02:43:00 +0900

    fix/dvh

--- src/components/form/FormSuccess.tsx
+++ src/components/form/FormSuccess.tsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 const FormSuccess = () => {
   return (
     <div
-      className={`min-h-[calc(100vh-var(--header-height-mobile)-5rem)] sm:min-h-[calc(100vh-var(--header-height-pc)-5rem)]`}
+      className={`min-h-[calc(100dvh-var(--header-height-mobile)-5rem)] sm:min-h-[calc(100dvh-var(--header-height-pc)-10rem)]`}
     >
       <div className="w-full max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-md text-center">
         <div className="my-8 flex flex-col items-center gap-4">
